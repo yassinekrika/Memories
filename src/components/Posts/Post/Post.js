027ecdbd10987ab04ab2acebede7a39702beb9eb
YassG4mer <yassinekrika2016@gmail.com>
2023-03-03 21:47:19 +0100

    post

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -1,12 +1,10 @@
 import React from "react";
-import { useSelector } from 'react-redux'
 import useStyles from './styles'
 
 const Post = () => {
-    const posts = useSelector((state) => {return state.posts})
+    
     const classes = useStyles()
 
-    console.log(posts)
 
     return (
         <h1 className={classes}>Post</h1>
